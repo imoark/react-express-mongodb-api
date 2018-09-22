78ef823afdd3fcec172a8d5d9ca59ba673fb5d21
imoark <ryomakuumi@gmail.com>
2018-09-21 19:41:35 -0700

    Fetching data from a remote API 1:10

--- api/index.js
+++ api/index.js
@@ -4,14 +4,16 @@ in the main server.js. Be modular!!
 So, we are going to manage all API requests in this module.*/
 import express from 'express';
 
+import data from '../src/testData';
+
 // we create a router object by calling the .Router() function in Express
 const router = express.Router();
 
 /* We will define .get() function/calls on the "router" object, and
 handle them in the second argument. Since this is gonna be an API call, 
 so we'll send a JSON response by sending an object here.*/
-router.get('/', (req,res) => {
-	res.send({ data: [] })
+router.get('/contests', (req,res) => {
+	res.send({ contests: data.contests });
 });
 
 // Finally, to be able to use the const "router" object, we need to export it
