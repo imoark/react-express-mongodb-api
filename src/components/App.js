759c84389f55ef214cb6542c0c7367874fa4ac13
imoark <ryomakuumi@gmail.com>
2018-09-21 20:41:48 -0700

    Reading from the state 1:48

--- src/components/App.js
+++ src/components/App.js
@@ -33,6 +33,18 @@ class App extends React.Component {
 			<Header message={this.state.pageHeader} />
 			<div>
 				{this.props.contests.map( contest =>
+						/* 
+						everytime we display a list of things dynamically,
+						REACT needs a little bit of help to identify every
+						element with a key. This key helps REACT identify
+						the element when this array of children changes.
+
+						In other words, everytime we have a map call, we
+						beed to provide a unique key to identify the child
+						element inside that map. ADVICE: Do not use the array
+						index as a unique key.
+						*/
+
 						<ContestPreview key={contest.id} {...contest} /> 
 					)
 				}
